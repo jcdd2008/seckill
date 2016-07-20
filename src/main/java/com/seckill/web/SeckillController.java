b715161214e9671551c669773f2760df9cbabdc6
niemengquan <niemengquan@126.com>
2016-07-20 15:42:57 +0800

    controller

---
+++ src/main/java/com/seckill/web/SeckillController.java
@@ -0,0 +1,109 @@
+package com.seckill.web;
+
+import com.seckill.dto.Exposer;
+import com.seckill.dto.SeckillExecution;
+import com.seckill.dto.SeckillResult;
+import com.seckill.entity.Seckill;
+import com.seckill.enums.SeckillStatEnum;
+import com.seckill.exception.RepeatKillException;
+import com.seckill.service.ISeckillService;
+import org.omg.CORBA.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by niemengquan on 2016/7/20.
+ */
+@Controller
+@RequestMapping(value = "/seckill")//url:/模块/资源/{id}/细分
+public class SeckillController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private ISeckillService seckillService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        //获取秒杀的列表页面
+        List<Seckill> seckillList = this.seckillService.getSeckillList();
+        model.addAttribute("list", seckillList);
+        return "list";
+    }
+
+    /**
+     * 秒杀详情页面
+     *
+     * @param seckillId
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/{seckillId}/detail")
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model) {
+        if (seckillId == null || "".equals(seckillId)) {
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = this.seckillService.getById(seckillId);
+        if (seckill == null) {
+            return "redirect:/seckill/list";
+        }
+        model.addAttribute("seckill", seckill);
+        return "detail";
+    }
+
+    /**
+     * 获取秒杀地址
+     *
+     * @return
+     */
+    @RequestMapping(value = "/{seckillId}/exposer", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(@PathVariable("seckillId") Long seckillId) {
+        SeckillResult<Exposer> result;
+        try {
+            Exposer exposer = this.seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true, exposer);
+        } catch (Exception e) {
+            result = new SeckillResult<Exposer>(false, e.getMessage());
+        }
+        return result;
+    }
+
+    /**
+     * 执行秒杀
+     *
+     * @return
+     */
+    @RequestMapping(value = "/{seckillId}/{md5}/execution", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId") Long seckillId, @PathVariable("md5") String md5, @CookieValue("userPhone") Long userPhone) {
+        if(userPhone==null||"".equals(userPhone)){
+            return new SeckillResult<SeckillExecution>(false,"用户为登录");
+        }
+        SeckillExecution seckillExecution=null;
+        try{
+             seckillExecution= this.seckillService.execuSeckill(seckillId, userPhone, md5);
+            return new SeckillResult<SeckillExecution>(true,seckillExecution);
+        }catch (RepeatKillException e){
+            logger.error(e.getMessage());
+            return new SeckillResult<SeckillExecution>(true,seckillExecution);
+        }
+    }
+
+    /**
+     * 获取系统的当前时间
+     *
+     * @return
+     */
+    @RequestMapping(value = "/time/now", method = RequestMethod.GET)
+    @ResponseBody
+    public SeckillResult<Long> time() {
+        Date date = new Date();
+        return new SeckillResult<Long>(true, date.getTime());
+    }
+}

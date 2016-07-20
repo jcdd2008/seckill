1501f818a629e359ae8a71b0361d8ba5e69fd7cc
niemengquan <niemengquan@126.com>
2016-07-20 15:42:19 +0800

    修改

--- src/main/java/com/seckill/service/ISeckillService.java
+++ src/main/java/com/seckill/service/ISeckillService.java
@@ -7,6 +7,9 @@ package com.seckill.service;
 import com.seckill.dto.Exposer;
 import com.seckill.dto.SeckillExecution;
 import com.seckill.entity.Seckill;
+import com.seckill.exception.RepeatKillException;
+import com.seckill.exception.SeckillCloseException;
+import com.seckill.exception.SeckillException;
 
 import java.util.List;
 
@@ -43,5 +46,5 @@ public interface ISeckillService {
      * @param md5
      * @return
      */
-    SeckillExecution execuSeckill(long seckillId, long userPhone, String md5);
+    SeckillExecution execuSeckill(long seckillId, long userPhone, String md5) throws RepeatKillException,SeckillCloseException,SeckillException;
 }
